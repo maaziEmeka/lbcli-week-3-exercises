2125c94eec9ab6b33538369b370d9511b0562844
Maazi Emeka <maazigithub@gmail.com>
2025-03-31 10:08:58 -0400

    Add: get the beneficiary of a PSBT.

--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,7 @@
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+TRANSACTION=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+ADDRESS=$(bitcoin-cli -regtest decodepsbt $TRANSACTION | jq -r '.tx.vout[].scriptPubKey.address')
+echo "$ADDRESS"
