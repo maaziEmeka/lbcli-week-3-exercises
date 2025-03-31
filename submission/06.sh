66c603068114798855243b3513072f0b14936e5f
Maazi Emeka <maazigithub@gmail.com>
2025-03-31 10:07:59 -0400

    Add: get the hash of a PSBT.

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,6 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+TRANSACTION=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+HASH=$(bitcoin-cli -regtest decodepsbt $TRANSACTION | jq -r .tx.hash)
+echo "$HASH"
